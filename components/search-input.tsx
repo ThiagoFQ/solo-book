44fb4745aacf3a056f140418f8a678a6270b37aa
ThiagoFQ <thiago.ffqq@gmail.com>
2024-12-03 19:53:06 +0000

    feature: created debouncer hook

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -2,8 +2,18 @@
 
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState(name || "");
+
   return (
     <div className="relative">
       <Search className="absolute h-4 top-3 left-4 text-muted-foreground" />
