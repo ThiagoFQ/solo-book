09194eeb260074ae775b1ae349bafed854e711e7
ThiagoFQ <thiago.ffqq@gmail.com>
2024-12-10 22:51:44 +0000

    fix mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,10 +1,5 @@
 import { Sidebar } from "@/components/sidebar";
-import {
-  Sheet,
-  SheetContent,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 
 export const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
@@ -14,7 +9,6 @@ export const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <SheetTitle className="sr-only">Mobile Sidebar</SheetTitle>
         <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
